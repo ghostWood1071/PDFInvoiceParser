55b8e172b4a5e6956abcf04fcb6856a9ae9e3708
VAnh89 <vietanha9a2@gmail.com>
2023-07-10 14:30:20 +0700

    update

--- src/extractor/SEOJINAUTOInvoiceExtractor.ts
+++ src/extractor/SEOJINAUTOInvoiceExtractor.ts
@@ -9,20 +9,48 @@ export class SEOJINAUTOInvoiceExtractor extends PdfExtractor {
     this.docLines = this.getDocLines();
   }
 
+  protected unitArr: string[] = ["kg", "EA", "cái", "chiếc"];
+
+  // protected renderPage(pageData: any): string {
+  //   //check documents https://mozilla.github.io/pdf.js/
+  //   let render_options = {
+  //     //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
+  //     normalizeWhitespace: false,
+  //     //do not attempt to combine same line TextItem's. The default value is `false`.
+  //     disableCombineTextItems: false,
+  //   };
+
+  //   let renderText = (textContent: any) => {
+  //     //fs.writeFileSync('lol.json', JSON.stringify(textContent));
+  //     let lastY,
+  //       text = "";
+  //     for (let item of textContent.items) {
+  //       if (lastY == item.transform[5] || !lastY) {
+  //         text += "#" + item.str;
+  //       } else {
+  //         text += "\n" + item.str;
+  //       }
+  //       lastY = item.transform[5];
+  //     }
+  //     return text;
+  //   };
+
+  //   return pageData.getTextContent(render_options).then(renderText);
+  // }
+
   private async processLines(pageLines: string[]) {
     let result: PageContent = new PageContent();
     let parts: PagePart = PagePart.NONE;
 
     let indexLine = 0;
     let l = pageLines.length;
-    let dateArr = [];
 
     while (indexLine <= l) {
       if (pageLines[indexLine] == "(Sign):") {
         parts = PagePart.SERIAL;
         indexLine++;
         if (parts == PagePart.SERIAL) {
-          result.serial = pageLines[indexLine];
+          result.serial = pageLines[indexLine].replace(/#/g, "");
         }
 
         parts = PagePart.NO;
@@ -43,7 +71,7 @@ export class SEOJINAUTOInvoiceExtractor extends PdfExtractor {
         if (parts == PagePart.DATE) {
           let str: string = pageLines[indexLine];
           result.date = new Date(
-            `${str.slice(4, 8)}/${str.slice(2, 4)}/${str.slice(0, 2)}`
+            `${str.slice(6, 10)}/${str.slice(4, 6)}/${str.slice(0, 2)}`
           );
           break;
         }
@@ -111,7 +139,7 @@ export class SEOJINAUTOInvoiceExtractor extends PdfExtractor {
     }
 
     while (indexLine < l) {
-      if (pageLines[indexLine] == "ABC123=1x2") {
+      if (pageLines[indexLine] == "ABC#1#2#3=#1x#2") {
         parts = PagePart.TABLE;
         indexLine++;
         break;
@@ -122,27 +150,56 @@ export class SEOJINAUTOInvoiceExtractor extends PdfExtractor {
     while (indexLine < l - 1) {
       if (parts == PagePart.TABLE) {
         let newTableContent: TableContent = new TableContent();
+
+        let numStrRegex = /^\d[\#\.\,\d]*\d$/;
         let str = "";
-        while (indexLine < l) {
-          if (isNaN(+pageLines[indexLine])) {
-            str += pageLines[indexLine];
-            indexLine++;
-          } else break;
+
+        while (
+          parts == PagePart.TABLE &&
+          !numStrRegex.test(pageLines[indexLine])
+        ) {
+          str += pageLines[indexLine];
+          indexLine++;
+        }
+        for (let unit of this.unitArr) {
+          if (str.includes(unit)) {
+            newTableContent.product_name = str.slice(0, -unit.length);
+            newTableContent.unit = unit;
+            break;
+          }
         }
 
-        let closerIndex = str.lastIndexOf(")");
-        newTableContent.product_name = str.slice(0, closerIndex + 1);
+        var numArr = str.split("#");
+        var commaIndex = numArr.indexOf(",");
 
-        let totalString = str.slice(closerIndex + 1);
-        let extractedTotal = await this.extractTotal(totalString);
+        // if (numStrRegex.test(pageLines[indexLine])) {
+        //   newTableContent.quantity = +numArr
+        //     .slice(0, commaIndex + 2)
+        //     .filter((x) => x != ".")
+        //     .join("")
+        //     .replace(",", ".");
 
-        newTableContent.unit = extractedTotal.unit;
-        newTableContent.quantity = extractedTotal.quantity;
-        newTableContent.unit_price = extractedTotal.unitPrice;
-        newTableContent.total = extractedTotal.total;
+        //   numArr = numArr.slice(commaIndex + 2);
+
+        //   commaIndex = numArr.indexOf(",");
+
+        //   newTableContent.unit_price = +numArr
+        //     .slice(0, commaIndex + 2)
+        //     .filter((x) => x != ".")
+        //     .join("")
+        //     .replace(",", ".");
+
+        //   numArr = numArr.slice(commaIndex + 2);
+
+        //   newTableContent.total = +numArr
+        //     .filter((x) => x != ".")
+        //     .join("")
+        //     .replace(",", ".");
+        // }
 
         result.table.push(newTableContent);
-        indexLine++;
+        indexLine += 2;
+        break;
       } else break;
     }
 
@@ -151,9 +208,11 @@ export class SEOJINAUTOInvoiceExtractor extends PdfExtractor {
 
   async getResult() {
     let data = await this.docLines;
-    let result = await data?.map(
-      async (x) => (x = JSON.stringify(await this.processLines(x)))
-    );
+    // let result = await data?.map(
+    //   async (x) => (x = JSON.stringify(await this.processLines(x)))
+    // );
+
+    let result = data ? await this.processLines(data[1]) : null;
 
     return result;
   }
