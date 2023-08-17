98848703495e0f77c524c7257298e178095e6479
ghostWood1071 <thinhquangshin@gmail.com>
2023-08-17 11:48:17 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -34,24 +34,4 @@ import { meInvoice2Extractor } from "./extractor/meInvoice2Extractor";
 import { MeInvoice4Extractor } from "./extractor/meInvoice4Extractor";
 import { meInvoiceExtractor } from "./extractor/meInvoiceExtractor";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-let extractor = new Test(
-  "src/pdf/8-Aug, 11 Aug to 22 Aug.pdf"
-);
 
-extractor.saveRawText("Shindengen");
-=======
-let extractor = new VNPT4Extractor("src/pdf/POSCO_349.pdf");
-
-// extractor.saveRawText("vnpt4");
->>>>>>> a6dc2da0a5a5a3233ea68ba736554fbda060af0e
-=======
-// let extractor = new MeInvoice6Extractor("src/pdf/00000092 - Dương Quang.pdf");
-let extractor = new MeInvoice6Extractor("src/pdf/01GTKT0_0000842.pdf");
-
-// extractor.saveRawText("meinvoice6-0");
-// extractor.saveRawText("meinvoice6");
->>>>>>> 08468c92e8828ac7edc6388915bd7411b98e4cf3
-
-// extractor.getResult().then((res) => console.log(res));
